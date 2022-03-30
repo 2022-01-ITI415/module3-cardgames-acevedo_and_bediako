cf5122ef2e6cc0cb21042a70b2b24c9e119ed26d
RyanAnthony1204 <racevedo1204@gmail.com>
2022-03-30 17:41:58 -0400

    Adjusted Variables
    adjusted some of the game end variables.

--- Assets/Prospector/__Scripts/Prospector.cs
+++ Assets/Prospector/__Scripts/Prospector.cs
@@ -19,7 +19,7 @@ public class Prospector : MonoBehaviour {
 	public Vector2				fsPosRun = new Vector2(0.5f, 0.75f);
 	public Vector2				fsPosMid2 = new Vector2(0.4f, 1.0f);
 	public Vector2				fsPosEnd = new Vector2(0.5f, 0.95f);
-	public float				reloadDelay = 2f;
+	public float				reloadDelay = 5f;
 	public Text					gameOverText, roundResultText, highScoreText;
 
 
